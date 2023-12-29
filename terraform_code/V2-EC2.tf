60478b3b4983c81ace33e71a1402aa8a872346f5
Franco De Fabrizio <franco.defabrizio@gmail.com>
2023-12-29 11:40:10 +0100

    V2 terraform file

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -6,7 +6,7 @@ provider "aws" {
  resource "aws_instance" "demo-server" {
   ami           = "ami-079db87dc4c10ac91"
   instance_type = "t2.micro"
-  key_name       = "ddp"
+  key_name       = "dpp"
   security_groups = [ "demo-sg" ]
  }
 
